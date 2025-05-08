9620c2d7956ac6250eb6cb976da7b3f3de70adc8
Dominik <domijas96@gmail.com>
2025-05-08 09:48:13 +0200

    [Feat] Refactor ArticleObserver

--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -5,31 +5,9 @@ namespace App\Observers;
 use App\Jobs\ReindexArticles;
 use App\Models\Article;
 use App\Services\ElasticsearchService;
-use Elastic\Elasticsearch\Exception\AuthenticationException;
 
 class ArticleObserver
 {
-    /**
-     * Handle the Article "created" event.
-     */
-    public function created(Article $article): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Article "updated" event.
-     */
-    public function updated(Article $article): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Article "deleted" event.
-     *
-     * @throws AuthenticationException
-     */
     public function deleted(Article $article): void
     {
         $es = app(ElasticsearchService::class)->client();
@@ -42,22 +20,6 @@ class ArticleObserver
         } catch (\Exception $e) {
             \Log::info($e->getMessage());
         }
-    }
-
-    /**
-     * Handle the Article "restored" event.
-     */
-    public function restored(Article $article): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Article "force deleted" event.
-     */
-    public function forceDeleted(Article $article): void
-    {
-        //
     }
 
     public function saved(Article $article)

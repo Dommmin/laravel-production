811380f3ecfd7aa5aa0567cbc7e0db59f2a7bf51
Dominik <domijas96@gmail.com>
2025-05-09 13:58:01 +0200

    [Feat] Update chats

---
+++ tests/Browser/ChatTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Faker\Factory as Faker;
+
+class ChatTest extends DuskTestCase
+{
+    public function test_user_can_send_and_see_message_in_chat()
+    {
+        $this->browse(function (Browser $browser) {
+            $faker = Faker::create();
+            $sender = User::factory()->create(['password' => 'password']);
+            $recipient = User::factory()->create();
+            $message = $faker->sentence;
+
+            $browser->loginAs($sender)
+                ->visit('/chat?recipient_id=' . $recipient->id)
+                ->screenshot('chat-page-after-visit')
+                ->storeSource('chat-page-after-visit.html')
+                ->waitForText($recipient->name, 10)
+                ->assertSee($recipient->name)
+                ->type('input[name="message"]', $message)
+                ->press('Send')
+                ->waitForText($message)
+                ->assertSee($message);
+        });
+    }
+
+    public function test_user_can_switch_conversation_and_see_empty_state()
+    {
+        $this->browse(function (Browser $browser) {
+            $sender = User::factory()->create(['password' => 'password']);
+            $recipient1 = User::factory()->create();
+            $recipient2 = User::factory()->create();
+
+            $browser->loginAs($sender)
+                ->visit('/chat?recipient_id=' . $recipient1->id)
+                ->screenshot('chat-page-switch-1')
+                ->storeSource('chat-page-switch-1.html')
+                ->waitForText($recipient1->name, 10)
+                ->assertSee($recipient1->name)
+                ->visit('/chat?recipient_id=' . $recipient2->id)
+                ->screenshot('chat-page-switch-2')
+                ->storeSource('chat-page-switch-2.html')
+                ->waitForText($recipient2->name, 10)
+                ->assertSee($recipient2->name)
+                ->assertSee('No messages yet. Start a conversation!');
+        });
+    }
+}

031c138f386a84f9e8aaba7c12d43e76d2839c65
Dominik <domijas96@gmail.com>
2025-05-16 11:24:14 +0200

    [Fix]

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,11 +4,11 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY ?? 'local-app-key',
-    wsHost: 'localhost',
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: false,
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: window.location.hostname,
+    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
+    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
+    forceTLS: window.location.protocol === 'https:',
     enabledTransports: ['ws', 'wss'],
     disableStats: true,
 });

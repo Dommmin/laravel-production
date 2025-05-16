27622b8dcbe29777b14dd5697ea79ea847a0eec6
Dominik <domijas96@gmail.com>
2025-05-16 12:17:47 +0200

    [Feat] Add debug

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,6 +1,13 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
+console.log('Debug - Vite env variables:', {
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    host: import.meta.env.VITE_REVERB_HOST,
+    port: import.meta.env.VITE_REVERB_PORT,
+    scheme: import.meta.env.VITE_REVERB_SCHEME
+});
+
 window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',

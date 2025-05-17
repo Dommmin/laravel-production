94c10f7b22316b3b5b5fbb52a42fdea9dac42020
Dominik <domijas96@gmail.com>
2025-05-17 15:40:38 +0200

    [Fix]

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -13,11 +13,16 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+    wsPort: import.meta.env.VITE_REVERB_PORT || 40168,
+    wssPort: import.meta.env.VITE_REVERB_PORT || 40168,
+    forceTLS: false,
     enabledTransports: ['ws', 'wss'],
     disableStats: true,
     cluster: 'mt1',
-    encrypted: true
+    encrypted: false,
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
+        }
+    }
 });

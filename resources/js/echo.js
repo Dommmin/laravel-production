800c1db1d4b21154af65eac98acee5bba1bca6aa
Dominik <domijas96@gmail.com>
2025-05-20 09:30:34 +0200

    fix

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -15,7 +15,7 @@ window.Echo = new Echo({
     wsHost: window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
     enabledTransports: ['ws', 'wss'],
     disableStats: true,
     cluster: 'mt1',

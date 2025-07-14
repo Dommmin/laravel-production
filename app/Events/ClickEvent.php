ed82ea973bb31db1b0f93f0e3be5d47ea24c1137
Dominik <domijas96@gmail.com>
2025-07-14 22:53:02 +0200

    [Feat] Updates

--- app/Events/ClickEvent.php
+++ app/Events/ClickEvent.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ClickEvent implements ShouldBroadcast
+class ClickEvent implements ShouldBroadcastNow
 {
     use Dispatchable;
     use InteractsWithSockets;

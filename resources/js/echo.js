644d2e66d40f3383b3fb5d159c7397597ac13db6
Dominik <domijas96@gmail.com>
2025-07-14 21:15:04 +0200

    [Feat] Update Docker config

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,14 +5,12 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: window.location.hostname,
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
     enabledTransports: ['ws'],
     disableStats: true,
-    cluster: 'mt1',
-    encrypted: false,
     auth: {
         headers: {
             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
